492e8b005349e4b562a4bf3dc58e0e1dc9236068
jrlv16 <jl062705@sfr.fr>
2020-01-15 17:19:00 +0100

    database postgres

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -7,16 +7,6 @@ from django.test import TestCase
 
 class CommandTest(TestCase):
 
-    @patch('time.sleep', return_value=True)
-    def test_wait_for_db(self, ts):
-        """
-        Test waiting for db
-        """
-        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
-            gi.side_effects = [OperationalError] * 5 + [True]
-            call_command('wait_for_db')
-            self.assertEqual(gi.call_count, 6)
-
     def test_wait_for_db_ready(self):
         """
         Test waiting for db when db is ready
@@ -25,3 +15,13 @@ class CommandTest(TestCase):
             gi.return_value = True
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 1)
+
+    @patch('time.sleep', return_value=True)
+    def test_wait_for_db(self, ts):
+        """
+        Test waiting for db
+        """
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.side_effects = [OperationalError] * 5 + [True]
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 6)            
