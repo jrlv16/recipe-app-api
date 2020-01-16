aa04b5f84456b14e30602e915101f1378fd7eeb7
jrlv16 <jl062705@sfr.fr>
2020-01-16 18:21:26 +0100

    essai travis

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def sample_user(email='jl062705@sfr.fr', password='testpass'):
+    """
+    Create a sample user
+    """
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -48,3 +56,12 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
