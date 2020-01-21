2a9ed13e66667c860ad97cb9e9d5f976a204e9eb
jrlv16 <jl062705@sfr.fr>
2020-01-21 08:32:56 +0100

    deploy heroku

--- app/app/settings.py
+++ app/app/settings.py
@@ -80,6 +80,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
 DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 ''' DATABASES = {
     'default': {
