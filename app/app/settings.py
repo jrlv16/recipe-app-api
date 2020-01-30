c461c26a9e11970b1e8d98598c10cc50f967bc2a
jrlv16 <jl062705@sfr.fr>
2020-01-30 12:14:13 +0100

    added media and static files recipeimage

--- app/app/settings.py
+++ app/app/settings.py
@@ -125,11 +125,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/vol/web/static')
+
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, '/vol/web/media')
+# STATIC_ROOT = '/vol/web/static'
 
 AUTH_USER_MODEL = 'core.user'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
